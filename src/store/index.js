c68986e7331a7ea9c8d56124ec84b5f4a3927020
Guilherme Girardi <girardi.gui@icloud.com>
2020-08-12 14:40:08 +0100

    Edit store

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,14 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import sagas from './sagas/index';
 import reducers from './ducks/index';
 
-const middlewares = [];
+const sagaMiddleware = createSagaMiddleware();
 
-const sagaMonitor = process.env.NODE_ENV !== 'development' ? console.tron.createSagaMiddleware() : null;
-
-const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
-
-middlewares.push(sagaMiddleware);
-
-const createAppropriateStore = process.env.NODE_ENV !== 'development' ? console.tron.createStore : createStore;
-
-const store = createAppropriateStore(reducers, compose(applyMiddleware(...middlewares)));
+const store = createStore(
+  reducers,
+  applyMiddleware(sagaMiddleware)
+);
 
 sagaMiddleware.run(sagas);
 
